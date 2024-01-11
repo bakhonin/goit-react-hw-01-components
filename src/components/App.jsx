fbd31298eeed42ff11a682efb1ac37c576a0e5e4
Denys <bakhonin1@gmail.com>
2024-01-11 09:06:11 +0100

    update vite.config, add prettierrc.json

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,24 +2,22 @@ import './App.css';
 import { Profile } from './Profile';
 import { FriendList } from './FriendList';
 import { TransactionHistory } from './TransactionHistory';
-import userData from "../data/userData.json";
-import friends from "../data/friends.json";
-import transactions from "../data/transactions.json";
-
-
+import userData from '../data/userData.json';
+import friends from '../data/friends.json';
+import transactions from '../data/transactions.json';
 
 export function App() {
   return (
-    <div className='container'>
-      <Profile 
-      name={userData.username}
-      tag={userData.tag}
-      location={userData.location}
-      image={userData.avatar}
-      stats={userData.stats}
+    <div className="container">
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
       />
-       <FriendList friends={friends} />
-       <TransactionHistory items={transactions} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 }
